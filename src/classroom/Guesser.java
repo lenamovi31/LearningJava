e2de1841874615ec336e69961c6cf8b46e885b4e
J.A. <jelena.adamovicha@gmail.com>
2024-03-14 12:04:36 +0200

    [classroom/11032024]: Update Classroom progress!

---
+++ src/classroom/Guesser.java
@@ -0,0 +1,13 @@
+//package classroom;
+//
+//import java.util.Random;
+//import java.util.Scanner;
+//
+//public class Guesser {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        Random random = new Random();
+//        randomNumber.nextInt( bound: 10) + 1;
+//
+//    }
+//}
